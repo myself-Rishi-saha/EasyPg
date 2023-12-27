28f6ee36397a706a7a7e1cb4565db84d42d80430
Sayan <sayanpatra017@gmail.com>
2023-12-28 02:48:15 +0530

    login-user

--- route.js
+++ route.js
@@ -5,9 +5,9 @@ router.get('/', (req,res)=>{
     // res.sendfile(path.join(__dirname,'index.html'))
       res.render('index')
 });
-// router.get('/login', (req,res)=>{
-//     // res.sendfile(path.join(__dirname,'index.html'))
-//       res.send("login")
-// });
+router.get('/loginUser', (req,res)=>{
+   
+      res.render("loginUser")
+});
 
 module.exports=router;

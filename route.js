08ae83f01f26a8ffba52f3eb01d21b97a3cdd56b
Sayan <sayanpatra017@gmail.com>
2023-12-28 23:50:20 +0530

    without sign up button

--- route.js
+++ route.js
@@ -3,11 +3,15 @@ var router=express.Router();
 
 router.get('/', (req,res)=>{
     // res.sendfile(path.join(__dirname,'index.html'))
-      res.render('signup')
+      res.render('index')
 });
 router.get('/loginUser', (req,res)=>{
    
       res.render("loginUser")
+});
+router.get('/signUpUser', (req,res)=>{
+   
+      res.render("signup")
 });
 
 module.exports=router;
